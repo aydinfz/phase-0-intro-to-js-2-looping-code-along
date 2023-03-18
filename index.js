9037cfd16c9d64ee64bd245a38acbe35c8d0e9e2
Zehra <aydin.fzehra@gmail.com>
2023-03-18 08:57:09 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const writeCards = (people, day) => {
+  const arr = [];
+  for (let i = 0; i < people.length; i++) {
+    arr.push(`Thank you, ${people[i]}, for the wonderful ${day} gift!`);
+  }
+  return arr;
+};
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+const countDown = (number) => {
+  while (number >= 0) {
+    console.log(number--);
+  }
+};
+countDown(10);
